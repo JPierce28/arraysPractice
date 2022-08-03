c3cd3db2b3c7ba695dfdccc51bb9200869b72f3e
Josh Pierce <joshua.alan.pierce@gmail.com>
2022-08-03 10:42:10 -0600

    Add three arrays

---
+++ arrays.js
@@ -0,0 +1,4 @@
+var favoriteFoods = ["Burgers", "Pizza", "Wings", "Steak"];
+var luckyNumbers = ["28", "4", "12", "15"];
+var questionNumber = 4;
+var questionAnswers = Array.apply(null, new Array(questionNumber)).map(function(){return false});
